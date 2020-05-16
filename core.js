0e516215490f8d1c93cc7eac922f90bb68acee97
Grey <andersongcg@gmail.com>
2020-05-16 12:30:50 -0400

    Refactor e adição de gorjeta ao calculo

--- core.js
+++ core.js
@@ -1,34 +1,66 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     
-    persistValue("vTotalVal", "totVal");
-    persistValue("nPessoasVal", "nbPessoas");
-    persistValue("cadaUmPagaVal", "totCada");
+    PersistValues();
     
 });
 
-function persistValue(sessionVarName, inputId) {
-    if (!sessionStorage.getItem(sessionVarName)) {
-        document.querySelector("#" + inputId).value = "";
+function Calcular() {
+    
+    let tipAmount = 0;
+    let billAmount = parseFloat(document.querySelector("#billAmount").value);
+    let nbPaying = parseFloat(document.querySelector("#nbPaying").value);
+    if (!nbPaying || nbPaying === 0) {
+        nbPaying = 1;
+    }
+    
+    let isAddTip = document.getElementById("isAddTip").checked;
+    let tipBase = parseFloat(document.querySelector("#tipBase").value);
+    if (isAddTip) {
+        let tipType = parseInt(getRadioSelectedOption("tipType"));
+        tipAmount = tipType === 1 ? billAmount * (tipBase / 100) : tipBase;
+    }
+    
+    let eachAmount = (billAmount + tipAmount) / nbPaying;
+    
+    sessionStorage.setItem("billAmountVal", billAmount);
+    sessionStorage.setItem("nbPayingVal", nbPaying);
+    sessionStorage.setItem("isAddTipVal", isAddTip);
+    sessionStorage.setItem("tipBaseVal", tipBase);
+    sessionStorage.setItem("tipAmountVal", tipAmount);
+    sessionStorage.setItem("eachAmountVal", eachAmount);
+    
+}
+
+function PersistValues() {
+    if (sessionStorage.length === 0) {
         return;
     }
 
-    document.querySelector("#" + inputId).value = sessionStorage.getItem(sessionVarName);
-    sessionStorage.removeItem(sessionVarName);
+    for (key in sessionStorage) {
+        if (typeof sessionStorage[key] === "function") {
+            continue;
+        }
+
+        let persistCtrl = document.getElementById(key.substring(0, key.length - 3));
+        if (!persistCtrl) {
+            continue;
+        } 
+        
+        persistCtrl.type === "checkbox" ? persistCtrl.checked = (sessionStorage[key] == "true") : persistCtrl.value = sessionStorage[key];
+    }
 }
 
-let Calcular = () => {
+function getRadioSelectedOption(radioElementName) {
     
-    let vTotal = document.querySelector("#totVal").value;
-    let nPessoas = document.querySelector("#nbPessoas").value;
+    let radioCtrl = document.getElementsByName(radioElementName);
     
-    if (!nPessoas || nPessoas === 0) {
-        nPessoas = 1;
+    if (!radioCtrl) {
+        return null;
     }
     
-    let cadaUmPaga = vTotal / nPessoas;
-    
-    sessionStorage.setItem("vTotalVal", vTotal);
-    sessionStorage.setItem("nPessoasVal", nPessoas);
-    sessionStorage.setItem("cadaUmPagaVal", cadaUmPaga);
-    
+    for (opt in radioCtrl) {
+        if (radioCtrl[opt].checked) {
+            return radioCtrl[opt].value;
+        }
+    }
 }
